42a8c9b4437912aff94f3e7d733eb316e3e121e3
RampageCrumpet <34409288+RampageCrumpet@users.noreply.github.com>
2023-12-22 12:22:45 -0600

    Added in Navmesh for pathfinding calculations.
    Added in navmesh for pathfinding calculations.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -10,15 +11,22 @@ public class GameController : NetworkBehaviour
     /// </summary>
     private LevelGeneratorController levelGeneratorController;
 
+    /// <summary>
+    /// The navmesh surface we want to use to use for pathfinding.
+    /// </summary>
+    private NavMeshSurface navMeshSurface;
+
     public override void OnNetworkSpawn()
     {
         if (this.IsHost || this.IsServer)
         {
             levelGeneratorController.GenerateLevelAcrossNetwork((int)Time.time);
+            navMeshSurface.BuildNavMesh();
         }
         else
         {
             levelGeneratorController.RequestLevelGenerationServerRpc();
+            UpdateNavmeshServerRpc();
         }
         base.OnNetworkSpawn();
     }
@@ -27,11 +35,18 @@ public class GameController : NetworkBehaviour
     void Start()
     {
         levelGeneratorController = this.GetComponentInChildren<LevelGeneratorController>();
+        navMeshSurface = this.GetComponentInChildren<NavMeshSurface>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    [ServerRpc]
+    private void UpdateNavmeshServerRpc()
+    {
+        navMeshSurface.BuildNavMesh();
     }
 }
